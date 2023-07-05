15f2fed017a680bb56a79220afae742520f9b771
Friambudi Ramadhan <sfriambudi@gmail.com>
2023-07-05 15:35:03 +0700

    update tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
 module.exports = {
   content: ['./index.html'],
   theme: {
@@ -10,14 +11,23 @@ module.exports = {
         400: '#A9A8A8',
         500: '#5B5B5B',
         600: '#363636',
+        700: '#2b2b2b'
       },
       blue: {
         100: '#ECEFFF',
         500: '#0D51A0',
       },
-      green: {
+      teal: {
         200: '#00CAA7'
-      }
+      },
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+      green: colors.green,
+      red: colors.red
     },
     extend: {},
   },
